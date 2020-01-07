808bf5b728dcee3fabd0b15ed19c5f9a005e15bd
infoey <32243391+infoey@users.noreply.github.com>
2020-01-07 23:14:25 +0100

    Update jquery-1.2.0.js

--- jquery-1.2.0.js
+++ jquery-1.2.0.js
@@ -1,5 +1,10 @@
-$("head").append("<link href='//infoey.github.io/infoey/plugins-rtl.min.css' rel='stylesheet'/>");
 $("head").append("<script src='//infoey.github.io/infoey/plugins-rtl.min.js'/>");
+var head = document.getElementsByTagName("HEAD")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "//infoey.github.io/infoey/plugins-rtl.min.css";
+head.appendChild(link);
 const html = document.querySelector("html");
 const daynight = document.querySelector(".night-day");
 const input = document.querySelector(".switch");
